6f3b032667c8dcb819ea5f5ceb3c1928ec7a7f5b
pranab <pranabpl@gmail.com>
2017-07-26 10:52:51 +0530

    New test project...

---
+++ TestProject/src/com/test/TestClass.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.test;
+
+/**
+ * @author PranabCP
+ *
+ */
+public class TestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hi git...");
+
+	}
+
+}
